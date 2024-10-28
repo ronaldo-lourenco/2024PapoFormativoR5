680675360ab2c83ad435579ccec620295dee4bb4
ronaldo-lourenco <ronaldo.lourenco@escola.pr.gov.br>
2024-10-28 17:52:15 +0000

    Atualizei informacoesGlobais

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,7 +7,7 @@ async function vizualizarInformacoesGlobais() {
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const minutos = Math.round((dados.tempo_medio - horas) * 60)
     const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2);
 
     const paragrafo = document.createElement('p')
